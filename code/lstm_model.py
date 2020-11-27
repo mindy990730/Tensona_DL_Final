0032c75bc7ab25ab69369a57acb9894ab2ab7584
Mindy <mindy990730@gmail.com>
2020-11-27 22:52:28 +0800

    commit
    some error when commiting to master from mindy

---
+++ code/lstm_model.py
@@ -0,0 +1,208 @@
+from preprocess import Data
+# from attention import *
+import numpy as np
+import pickle
+import linecache
+import math
+import tensorflow as tf
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+class lstm_source(tf.keras.Model):
+    def __init__(self, params):
+
+        super(lstm_source, self).__init__()
+
+        # self.self_attention = Atten_Head(params.embed_size,params.embed_size,use_mask=False)
+        # self.layer_norm = tf.keras.layers.LayerNormalization(axis=-1)
+
+        hidden_sz = params.hidden_sz
+        self.lstm_s_1 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_1 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_s_2 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_2 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_s_3 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_3 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_s_4= tf.keras.layers.LSTM(hidden_sz, activation='relu', return_state=True, return_sequences=True)
+        self.dense_1 = tf.keras.layers.Dense(hidden_sz, activation='relu')
+        self.dense_2 = tf.keras.layers.Dense(params.embed_size, activation=None)
+        # print('lstm_source built!!!\n')
+    
+    def call(self, input, initial_state):
+        """
+
+        Forward pass of source LSTM.
+
+        :param input: source embedding input, shape = (batch_size, sentence_max_length, embed_size)
+        :param initial_state: hidden state from previous executions of call
+
+        :return output: output of 4 layers of LSTM, shape = (batch_size, sentence_max_length, embed_size)
+        :       initial_state: the last hidden state, to be used as initial state of lstm_target
+        """
+        # TODO: create self attention 
+
+
+
+        output, h, c = self.lstm_s_1(input, initial_state=initial_state)
+        initial_state = (h,c)
+        # output = self.dropout_1(output)
+        output, h, c = self.lstm_s_2(output, initial_state=initial_state)
+        initial_state = (h,c)
+        # output = self.dropout_2(output)
+        output, h, c = self.lstm_s_3(output, initial_state=initial_state)
+        # output = self.dropout_3(output)
+        print('lstm_s_3 output shape = ', tf.shape(output), '\n')
+        initial_state = (h,c)
+        output, h, c = self.lstm_s_4(output, initial_state=initial_state)
+        initial_state = (h,c)
+        # print('lstm_s_4 output shape = ', tf.shape(output), '\n')
+        out = self.dense_1(output)
+        # print('dense_1 output shape = ', tf.shape(output), '\n')
+        out = self.dense_2(out)
+        # print('dense_2 output shape = ', tf.shape(output), '\n')
+        # print('lstm source output: ', output, '\n\n')
+        return out, initial_state
+
+
+class lstm_target(tf.keras.Model):
+    def __init__(self, params, num_vocab, num_characters, mode='SPEAKER'):
+
+        super(lstm_target, self).__init__()
+        hidden_sz = params.hidden_sz
+        self.persona_embedding = tf.random.normal([num_characters, params.embed_size], stddev=.1, dtype=tf.float32)
+        # print('persona_embedding = ', self.persona_embedding)
+        self.lstm_t_1 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_1 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_t_2 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_2 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_t_3 = tf.keras.layers.LSTM(hidden_sz, activation='relu', dropout=params.dropout, return_state=True, return_sequences=True)
+        # self.dropout_3 = tf.keras.layers.Dropout(params.dropout)
+        self.lstm_t_4= tf.keras.layers.LSTM(hidden_sz, activation='relu', return_state=True, return_sequences=True)
+        self.dense = tf.keras.layers.Dense(num_vocab, activation='softmax')
+        self.mode = mode
+        # print('lstm_target built!!!!!!\n')
+
+    def call(self, encoded_output, initial_state, word_embedding, speaker, addressee):
+        """
+
+        Forward pass of target LSTM that calculates the probablity of next words. 
+
+        :param encoded_output: output of lstm_source, shape = (batch_size, sentence_max_length, embed_size)
+        :param initial_state: last hidden_state of lstm_source or previous calls of lstm_target
+        :param word_embedding: target embedding input, shape = (batch_size, 1, embed_size)
+        :param speaker: list of speakers in this batch, shape = (batch_size)
+        :param addressee: list of addressee in this batch, shape = (batch_size)
+
+        :return probs: probilities of vocab for this batch of words, shape = (batch_size, num_vocab)
+        :       initial_state: the last hidden state of this call; to be used in the next call
+        """
+        speaker_embed = tf.nn.embedding_lookup(self.persona_embedding, speaker) # shape = (batch_size, embed_size)
+        speaker_embed = tf.expand_dims(speaker_embed, axis=1) # shape = (batch_size, 1, embed_size)
+        # print('speaker embedding size = ', speaker_embed)
+        word_embedding = tf.expand_dims(word_embedding, axis=1)
+        # print('word embedding size = ', word_embedding)
+        lstm_t_input = tf.concat([word_embedding, speaker_embed], axis=2) # shape = (batch_size, 1, 2*embed_size)
+        # if self.mode=='SPEAKER_ADDRESSEE':
+        #     addressee_embed = tf.nn.embedding_lookup(self.persona_embedding, addressee) # shape = (batch_size, embed_size)
+        #     lstm_t_input = tf.concat([lstm_t_input, addressee_embed], axis=1) # shape = (batch_size, 1, 3*embed_size)
+
+        output, h, c = self.lstm_t_1(lstm_t_input, initial_state=initial_state)
+        initial_state = (h,c)
+        # output = self.dropout_1(output)
+        output, h, c = self.lstm_t_2(output, initial_state=initial_state)
+        initial_state = (h,c)
+        # output = self.dropout_2(output)
+        output, h, c = self.lstm_t_3(output, initial_state=initial_state)
+        initial_state = (h,c)
+        # output = self.dropout_3(output)
+        output, h, c = self.lstm_t_4(output, initial_state=initial_state)
+        initial_state = (h,c)
+        probs = self.dense(output) # shape = (batch_size, 1, num_vocab)
+        probs = tf.squeeze(probs)
+        return probs, initial_state
+
+
+class lstm_model(tf.keras.Model):
+
+    def __init__(self, params, num_vocab, num_characters):
+        super(lstm_model, self).__init__()
+        self.encoder = lstm_source(params)
+        self.decoder = lstm_target(params, num_vocab, num_characters, mode='SPEAKER')
+        self.source_embedding = tf.random.normal([num_vocab, params.embed_size], stddev=.1, dtype=tf.float32)
+        self.target_embedding = tf.random.normal([num_vocab, params.embed_size], stddev=.1, dtype=tf.float32)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = params.lr_rate)
+        # print('lstm_model is built!!!!!!!!!!!!!\n\n')
+
+    def call(self, batched_source, batched_target, speaker_list, addressee_list):
+        """
+
+        Runs the model on one batch of source & target inputs
+
+        :param batched_source: 2-D array of size (batch_size, sentence_max_length) that contains the batched, tokenized source scripts 
+        :param batched_target: 2-D array of size (batch_size, sentence_max_length) that contains the batched, tokenized target scripts 
+        :param speaker_list: 1-D array of size (batch_size) that contains speaker ids
+        :param addressee_list: 1-D array of size (batch_size) that contains addressee ids
+
+        :return loss: a tensor that contains the loss of this batch
+        :       probs_list: a 2-D tensor that contains probabilities calculated for each column of 
+                            words in target, shape = (sentence_max_length-1, batch_size, num_vocab)
+
+        """
+        # print(batched_target)
+        source_ebd = tf.nn.embedding_lookup(self.source_embedding, batched_source) # shape = (batch_size, sentence_max_length, embed_size)
+        # run LSTM encoder on the source sentence
+        encoded_output, initial_state = self.encoder(source_ebd, initial_state=None)
+        losses = []
+        probs_list = []
+        # Going horizontally by columns and predict one word each step; compare loss with target next word
+        for i in range(tf.shape(batched_target)[1]-1):
+            target_ebd = tf.nn.embedding_lookup(self.target_embedding, batched_target[:, i]) # shape = (batch_size, 1, embed_size)
+            probs, initial_state = self.decoder(encoded_output, initial_state, target_ebd, speaker_list, addressee_list)
+            labels = tf.squeeze(batched_target[:, i+1]) # shape = (batch_size,)
+            l = self.loss_func(probs, labels)
+            print('loss in the batch = ', l, '\n')
+            losses.append(l)
+            probs_list.append(probs)
+        # probs_list shape = [(num_col of batched_target -1), batch_size, num_vocab (probs of each vocab)]
+        # labels shape = [(num_col of batched_target -1), batch_size] (transpose of batched_target except the first column of batched_target)
+        losses = tf.convert_to_tensor(losses) 
+        # print('\n\n', losses, '\n\n')
+        loss = tf.reduce_sum(losses)
+        probs_list = tf.convert_to_tensor(probs_list)
+        return loss, probs_list
+
+    def loss_func(self, probs, labels):
+        """
+        Computes the loss of this columns of words
+        :param probs:  float tensor, word prediction probabilities (batch_size, num_vocab)
+        :param labels:  integer tensor, word prediction labels (batch_size,)
+        :return: scalar tensor of accuracy of the batch between 0 and 1
+        """
+        # print('loss function in ======================================')
+        # probs = tf.squeeze(probs)
+        l = tf.keras.losses.sparse_categorical_crossentropy(labels, probs)
+        l = tf.math.reduce_sum(l)
+        # print(l)
+        # print('loss function out ======================================')
+        return l
+
+    def accuracy_function(self, probs, labels):
+        """
+        Computes the batch accuracy
+        :param probs:  a 3-D tensor that contains probabilities calculated for each column of words
+                        in target, shape = (sentence_max_length-1, batch_size, num_vocab)
+        :param labels: prediction of next word in target, shape = (sentence_max_length-1, batch_size)
+        :return: scalar tensor of accuracy of the batch between 0 and 1     
+        """
+        # Hardcoded, #TODO: Try beam search
+        decoded_vocabs = tf.cast(tf.argmax(input=probs, axis=2), dtype=tf.int64)
+        accuracy = tf.reduce_mean((tf.cast(tf.equal(decoded_vocabs, labels), dtype=tf.float32)))
+        return accuracy
+
+
+
+
+
+
+
--- code/preprocess.py
+++ code/preprocess.py
@@ -100,12 +100,127 @@ class Data():
             speaker_name = speakers[i]
             addressee_name = speakers[i+1]
             if speaker_name not in character_dict.keys():
+<<<<<<< Updated upstream
                 character_dict['speaker_name'] = count_characters
                 count_characters += 1
             if addressee_name not in character_dict.keys():
                 character_dict['addressee_name'] = count_characters
                 count_characters += 1
+=======
+                character_dict[speaker_name] = num_characters
+                num_characters += 1
+            if addressee_name not in character_dict.keys():
+                character_dict[addressee_name] = num_characters
+                num_characters += 1
+
+            # tokenize speaker scripts
+>>>>>>> Stashed changes
             speaker_scripts = tokens[i]
             addressee_scripts = tokens[i+1]
+<<<<<<< Updated upstream
             spkr_adrs_list.append([character_dict['speaker_name'], speaker_scripts, character_dict['addressee_name'], addressee_scripts])
         return spkr_adrs_list, character_dict 
+=======
+            tokenized_addressee_scripts = []
+            for sentence in addressee_scripts: 
+                tokenized_addressee_scripts.extend(sentence)
+            if len(tokenized_addressee_scripts) > self.params.sentence_max_length-2: # account for EOS & EOT
+                # only keep sentence_max_length number of words at most
+                tokenized_addressee_scripts = tokenized_addressee_scripts[:self.params.sentence_max_length]
+            for j in range(len(tokenized_addressee_scripts)):
+                word = tokenized_addressee_scripts[j]
+                if word not in vocab_dict.keys():
+                    vocab_dict[word] = num_vocab
+                    num_vocab += 1
+                tokenized_addressee_scripts[j] = vocab_dict[word]
+            if len(tokenized_addressee_scripts) < self.params.sentence_max_length:
+                extension = np.zeros(self.params.sentence_max_length-len(tokenized_addressee_scripts))
+                tokenized_addressee_scripts.extend(extension)
+            
+            # Add EOS & EOT to tokenized_addressee_scripts
+            tokenized_addressee_scripts = [self.EOS] + tokenized_addressee_scripts + [self.EOT] 
+            spkr_adrs_list.append([character_dict[speaker_name], tokenized_speaker_scripts, character_dict[addressee_name], tokenized_addressee_scripts])
+        
+        # Update hyperparameters of vocab & character dict
+        self.vocab_dict = vocab_dict
+        self.character_dict = character_dict
+        # print()
+        return spkr_adrs_list
+
+    def train_test_split(self, all_data, p_split=0.9):
+        """
+        Shuffle and split the data into train_data and test_data 
+        :param all_data: 2-D array with each row = [speaker_id, speaker_scripts, addressee_id, addressee_scripts]
+        :param p_split: percentage of data to be train_data (default=0.9)
+        :return train_data: 2-D tensor with each row = [speaker_id, speaker_scripts, addressee_id, addressee_scripts]
+        :       test_data: 2-D tensor with each row = [speaker_id, speaker_scripts, addressee_id, addressee_scripts]
+        """
+        # Convert all data into speakers, speaker_scripts, addressee, addressee_scripts
+        speakers = []
+        speaker_scripts = []
+        addressee = []
+        addressee_scripts = []
+        # print(all_data, '\n\n')
+        for row in range(len(all_data)):
+            speakers.append(all_data[row][0])
+            speaker_scripts.append(all_data[row][1])
+            addressee.append(all_data[row][2])
+            addressee_scripts.append(all_data[row][3])
+        speakers = tf.squeeze(speakers)
+        speaker_scripts = tf.squeeze(speaker_scripts)
+        addressee = tf.squeeze(addressee)
+        addressee_scripts = tf.squeeze(addressee_scripts)
+        # Shuffle data
+        indices = tf.range(0, len(all_data), 1)
+        indices = tf.random.shuffle(indices)
+        # ==================================================================
+        # all data of mixed type & cannot be converted to tensor (change into speaker, scripts here and shuffle each separately)
+        # all_data = tf.convert_to_tensor(all_data)
+        speakers = tf.gather(speakers, indices, axis=0)
+        speaker_scripts = tf.gather(speaker_scripts, indices, axis=0)
+        addressee = tf.gather(addressee, indices, axis=0)
+        addressee_scripts = tf.gather(addressee_scripts, indices, axis=0)
+    
+        # Split into train and test 
+        num_train = int(len(speakers) * 0.9)
+        num_test = tf.shape(speakers)[0] - num_train
+        
+        train_speakers = tf.cast(tf.slice(speakers, [0], [num_train]), dtype=tf.int64)
+        test_speakers = tf.cast(tf.slice(speakers, [num_train], [num_test]), dtype=tf.int64)
+        
+        train_speaker_scripts = tf.cast(tf.slice(speaker_scripts, [0, 0], [num_train, self.params.sentence_max_length]), dtype=tf.int64)
+        test_speaker_scripts = tf.cast(tf.slice(speaker_scripts, [num_train, 0], [num_test, self.params.sentence_max_length]), dtype=tf.int64)
+        
+        train_addressees = tf.cast(tf.slice(speakers, [0], [num_train]), dtype=tf.int64)
+        test_addressees = tf.cast(tf.slice(speakers, [num_train], [num_test]), dtype=tf.int64)
+
+        train_addressee_scripts = tf.cast(tf.slice(addressee_scripts, [0, 0], [num_train, self.params.sentence_max_length]), dtype=tf.int64)
+        test_addressee_scripts = tf.cast(tf.slice(addressee_scripts, [num_train, 0], [num_test, self.params.sentence_max_length]), dtype=tf.int64)
+        print('number of train data = ', len(train_addressees))
+        test_data = [test_speakers, test_speaker_scripts, test_addressees, test_addressee_scripts]
+        train_data = [train_speakers, train_speaker_scripts, train_addressees, train_addressee_scripts]
+
+        return train_data, test_data
+
+    def read_batch(self, data, start_index, mode='train'):
+        """
+        Use the helper functions in this file to read and parse training and test data, then pad the corpus.
+        Then vectorize your train and test data based on your vocabulary dictionaries.
+
+        :param data: train or test data, a 2-D array of size (train_sz/test_sz, 4) each row with form [speaker_id, speaker_scripts, addressee_id, addressee_scripts]
+        :param start_index: starting index of rows in spkr_adrs_list.
+        :param mode: #TODO
+        :return sources: 2-D array of size (batch_size, sentence_max_length) that contains the batched, tokenized source scripts 
+        :       targets: 2-D array of size (batch_size, sentence_max_length) that contains the batched, tokenized target scripts 
+        :       speakers: 1-D array of size (batch_size) that contains speaker ids
+        :       addressees: 1-D array of size (batch_size) that contains addressee ids
+        """
+        s = data[1]
+        t = data[3]
+        sources = s[start_index:(start_index + self.params.batch_size)]
+        targets = t[start_index:(start_index + self.params.batch_size)]
+        speakers = data[0][start_index:(start_index + self.params.batch_size)]
+        addressees = data[2][start_index:(start_index + self.params.batch_size)]
+
+        return sources, targets, speakers, addressees
+>>>>>>> Stashed changes
